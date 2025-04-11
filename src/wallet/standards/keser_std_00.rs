e681ff0201e0727f8a2d8eadb86b57a2fda29ff7
silene0259 <195784300+YugenSource@users.noreply.github.com>
2025-04-11 14:27:58 -0400

    Update keser_std_00.rs

--- src/wallet/standards/keser_std_00.rs
+++ src/wallet/standards/keser_std_00.rs
@@ -17,12 +17,12 @@ use fixedstr::str64;
 pub struct KeserStandard;
 
 impl KeserStandard {
-    pub fn from_bytes(bytes: &[u8]) -> [u8;28] {
-        let mut fixed = [0u8;28];
-        let hasher = SlugBlake2sHasher::new(28);
+    pub fn from_bytes(bytes: &[u8]) -> [u8;32] {
+        let mut fixed = [0u8;32];
+        let hasher = SlugBlake2sHasher::new(32);
         let hash = hasher.hash(bytes);
 
-        if hash.len() == 28 {
+        if hash.len() == 32 {
             fixed.copy_from_slice(&hash);
         } else {
             panic!("Hash length mismatch");
